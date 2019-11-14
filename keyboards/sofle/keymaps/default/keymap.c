02c58a30b73ad56fe734a01697f662a90d401dcf
Josef Adamcik <josef.adamcik@gmail.com>
2019-11-14 22:39:25 +0100

    Use OLED support from QMK

--- keyboards/sofle/keymaps/default/keymap.c
+++ keyboards/sofle/keymaps/default/keymap.c
@@ -222,66 +222,62 @@ void matrix_init_user(void) {
     #endif
 }
 
-//SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
-#ifdef SSD1306OLED
+static void render_logo(void) {
+  static const char PROGMEM qmk_logo[] = {
+    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+    0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+    0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
 
-// When add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
-const char *read_mode_state(void);
-const char *read_logo(void);
-// void set_keylog(uint16_t keycode, keyrecord_t *record);
-// const char *read_keylog(void);
-// const char *read_keylogs(void);
-
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
-
-void matrix_scan_user(void) {
-   iota_gfx_task();
+  oled_write_P(qmk_logo, false);
 }
 
-void matrix_render_user(struct CharacterMatrix *matrix) {
+static void print_status(void) {
+  // Print current mode
+  switch (biton32(default_layer_state)) {
+  case _W_COLEMAK:
+    oled_write_P(PSTR("Mode: Colemak Linux\n"), false);
+    break;
+  case _M_COLEMAK:
+    oled_write_P(PSTR("Mode: Colemak Mac\n"), false);
+    break;
+  default:
+    oled_write_P(PSTR("Mode: Undefined\n"), false);
+  }
+  // Print current layer
+  oled_write_P(PSTR("Layer: "), false);
+  switch (biton32(layer_state)) {
+    case _M_COLEMAK:
+    case _W_COLEMAK:
+      oled_write_P(PSTR("Base\n"), false);
+      break;
+    case _M_RAISE:
+    case _W_RAISE:
+      oled_write_P(PSTR("Raise\n"), false);
+      break;
+    case _M_LOWER:
+    case _W_LOWER:
+      oled_write_P(PSTR("Lower\n"), false);
+      break;
+    case _ADJUST:
+      oled_write_P(PSTR("Adjust\n"), false);
+      break;
+    default:
+      oled_write_ln_P(PSTR("Undefined"), false);
+  }
+
+  oled_write_ln_P(PSTR(""), false);
+}
+
+void oled_task_user(void) {
   if (is_master) {
-    // If you want to change the display of OLED, you need to change here
-    matrix_write_ln(matrix, read_mode_state());
-    matrix_write_ln(matrix, read_layer_state());
-    // matrix_write_ln(matrix, read_keylog());
-    // matrix_write_ln(matrix, read_keylogs());
-    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
-    //matrix_write_ln(matrix, read_host_led_state());
-    // matrix_write_ln(matrix, read_timelog());
+//   if (is_keyboard_master()) {
+    print_status();
   } else {
-    // matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
-    // matrix_write_ln(matrix, read_timelog());
-    //matrix_write(matrix, read_logo());
+    render_logo();
   }
 }
-
-void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
-  if (memcmp(dest->display, source->display, sizeof(dest->display))) {
-    memcpy(dest->display, source->display, sizeof(dest->display));
-    dest->dirty = true;
-  }
-}
-
-void iota_gfx_task_user(void) {
-  struct CharacterMatrix matrix;
-  matrix_clear(&matrix);
-  matrix_render_user(&matrix);
-  matrix_update(&display, &matrix);
-}
-#endif//SSD1306OLED
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-#ifdef SSD1306OLED
-    // set_keylog(keycode, record);
-    // set_timelog();
-#endif
-  }
-
   switch (keycode) {
     case W_COLEMAK:
       if (record->event.pressed) {

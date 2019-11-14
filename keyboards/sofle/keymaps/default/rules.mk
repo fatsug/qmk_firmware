5f30ddbd8505a618452d3049a07f8bfd1802a7b2
Josef Adamcik <josef.adamcik@gmail.com>
2019-11-14 22:43:22 +0100

    Remove the OLED support and functional code from the project

--- keyboards/sofle/keymaps/default/rules.mk
+++ keyboards/sofle/keymaps/default/rules.mk
@@ -19,14 +19,3 @@ OLED_DRIVER_ENABLE = yes
 ENCODER_ENABLE = yes
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
-
-# If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/glcdfont.c \
-        ./lib/layer_state_reader.c \
-		./lib/mode_state_reader.c \
-        # ./lib/rgb_state_reader.c \
-        ./lib/logo_reader.c \
-        # ./lib/keylogger.c \
-        # ./lib/timelogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \

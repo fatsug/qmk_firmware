228377927efe51bcdb59c14eae4db7ecdcc36d07
Josef Adamcik <josef.adamcik@gmail.com>
2019-11-16 17:43:24 +0100

    cleaen unused custom implementation of everything

--- keyboards/sofle/rev1/config.h
+++ keyboards/sofle/rev1/config.h
@@ -40,20 +40,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* define tapping term */
 #define TAPPING_TERM 100
 
-/* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-//#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-//#define LOCKING_RESYNC_ENABLE
-
-/* ws2812 RGB LED */
-// #define RGB_DI_PIN D3
-// #define RGBLED_NUM 12    // Number of LEDs
 
 /* encoder support */
 #define ENCODERS_PAD_A { F5 }

1240b7e0d03686676ad93026f5f9b3e9817d7034
Josef Adamcik <josef.adamcik@gmail.com>
2020-03-07 10:18:39 +0100

    Various changes

--- keyboards/sofle/rules.mk
+++ keyboards/sofle/rules.mk
@@ -27,12 +27,10 @@ AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
-ENCODER_ENABLE = yes
 CUSTOM_MATRIX = no
 SPLIT_KEYBOARD = yes
-OLED_DRIVER_ENABLE = yes
+
 
 # if firmware size over limit, try this option
 # CFLAGS += -flto

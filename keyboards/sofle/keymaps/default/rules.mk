1631867e6d287418deeaa56f958208ff523b2cef
Josef Adamcik <josef.adamcik@gmail.com>
2020-03-07 09:37:45 +0100

    Add debugging, remove unecessary function for layer switching. Change encoders to page_up/down.

--- keyboards/sofle/keymaps/default/rules.mk
+++ keyboards/sofle/keymaps/default/rules.mk
@@ -19,3 +19,4 @@ OLED_DRIVER_ENABLE = yes
 ENCODER_ENABLE = yes
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+CONSOLE_ENABLE = yes

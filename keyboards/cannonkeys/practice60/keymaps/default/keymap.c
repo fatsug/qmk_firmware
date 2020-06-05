5ce20218908599a63cdff8e39641c150916a0c8f
Gus Kohtz <1nd13r0ck@gmail.com>
2020-06-05 14:37:23 -0500

    Update keymap.c
    sucessfully add Colemak layer

--- keyboards/cannonkeys/practice60/keymaps/default/keymap.c
+++ keyboards/cannonkeys/practice60/keymaps/default/keymap.c
@@ -22,10 +22,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _BASE 0
-#define _FN1 1
+#define _COLEMAK 1
+#define _FN1 2
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
+  KC_QWERTY = SAFE_RANGE,
+  KC_COLEMAK
 };
 
 /*
@@ -53,11 +55,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL,  KC_LGUI, KC_LALT,                        KC_SPC,                                   KC_RALT, KC_RGUI, MO(_FN1), KC_RCTL
   ),
 
+  [_COLEMAK] = LAYOUT_60_ansi(
+    KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC, \
+    KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC,  KC_BSLS, \
+    MO(_FN1), KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,           KC_ENT,  \
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                    KC_RSFT, \
+    KC_LCTL,  KC_LGUI, KC_LALT,                        KC_SPC,                                   KC_RALT, KC_RGUI, MO(_FN1), KC_RCTL
+  ),
+
   [_FN1] = LAYOUT_60_ansi(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
     KC_CAPS, RGB_MOD, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,          _______, \
-    _______,  BL_DEC,  BL_TOGG, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, \
+    _______,  BL_DEC,  BL_TOGG, _______, _______, _______, _______, _______, _______, KC_QWERTY, KC_COLEMAK,             _______, \
     _______,  _______, _______,                            _______,                            _______, _______, _______, _______
   )
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_QWERTY:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_BASE);
+            }
+            return false;
+        case KC_COLEMAK:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_COLEMAK);
+            }
+            return false;
+    }
+    return true;
+}

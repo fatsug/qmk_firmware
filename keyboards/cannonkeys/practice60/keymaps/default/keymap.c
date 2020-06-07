ca87c353c8db0e513418ef81ac84f977a5f7ae83
Gus Kohtz <1nd13r0ck@gmail.com>
2020-06-07 08:43:59 -0500

    Update keymap.c
    Rename FN1 layer to Lower. Replace Mod with APP key. Add Reset key to Raise layer.

--- keyboards/cannonkeys/practice60/keymaps/default/keymap.c
+++ keyboards/cannonkeys/practice60/keymaps/default/keymap.c
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // entirely and just use numbers.
 #define _BASE 0
 #define _COLEMAK 1
-#define _FN1 2
+#define _LOWER 2
 #define _RAISE 3
 
 enum custom_keycodes {
@@ -52,32 +52,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_60_ansi(
     KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC, \
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS, \
-    MO(_FN1), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,  \
+    MO(_LOWER), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,  \
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                    KC_RSFT, \
-    KC_LCTL,  KC_LGUI, KC_LALT,                        KC_SPC,                                   MO(_FN1), MO(_RAISE), MO(_FN1), KC_RCTL
+    KC_LCTL,  KC_LGUI, KC_LALT,                        KC_SPC,                                   MO(_LOWER), MO(_RAISE), KC_APP, KC_RCTL
   ),
 
   [_COLEMAK] = LAYOUT_60_ansi(
     KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC, \
     KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC,  KC_BSLS, \
-    MO(_FN1), KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,           KC_ENT,  \
+    MO(_LOWER), KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,           KC_ENT,  \
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                    KC_RSFT, \
-    KC_LCTL,  KC_LGUI, KC_LALT,                        KC_SPC,                                   MO(_FN1), MO(_RAISE), MO(_FN1), KC_RCTL
+    KC_LCTL,  KC_LGUI, KC_LALT,                        KC_SPC,                                   MO(_LOWER), MO(_RAISE), KC_APP, KC_RCTL
   ),
 
-  [_FN1] = LAYOUT_60_ansi(
+  [_LOWER] = LAYOUT_60_ansi(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
-    KC_CAPS, RGB_MOD, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    KC_CAPS, RGB_MOD, KC_UP,   _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______, _______, _______, _______, \
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,          _______, \
     _______,  BL_DEC, KC_DEL,  _______, _______, _______, _______, _______, _______, KC_QWERTY, KC_COLEMAK,              _______, \
     _______,  _______, _______,                            _______,                            _______, _______, _______, _______
   ),
 
   [_RAISE] = LAYOUT_60_ansi(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+      RESET, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,          _______, \
-    _______, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______, _______, _______, _______, KC_QWERTY, KC_COLEMAK,              _______, \
+    _______, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______, _______, _______, _______, KC_MS_BTN1, KC_MS_BTN2,             _______, \
     _______,  _______, _______,                            _______,                            _______, _______, _______, _______
   )
 };

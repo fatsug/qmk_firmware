6ea20b555ae5020b6210b02d4d698661037ff88e
Gus Kohtz <1nd13r0ck@gmail.com>
2020-08-22 13:30:42 -0500

    swap page up/down

--- keyboards/dztech/dz60rgb/keymaps/fatsug/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/fatsug/keymap.c
@@ -84,6 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,          KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, RSFT_T(KC_SLSH), KC_UP, LT(_LOWER,KC_DEL), \
         KC_LCTL,  KC_LGUI, KC_LALT,                        KC_SPC,                             KC_LOWER, KC_RAISE, KC_LEFT, KC_DOWN, KC_RGHT
     ),
+
 	//[1] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, KC_O, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_K, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
     [_COLEMAK] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______, \
@@ -96,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//[2] = LAYOUT(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_TRNS, RGB_TOG, KC_TRNS, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS, RESET, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_HOME, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_END, KC_TRNS, EEP_RST, KC_TRNS, KC_MPLY, KC_MRWD, KC_MFFD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_TRNS),
     [_LOWER] = LAYOUT(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,        KC_DEL, \
-        KC_CAPS,     RGB_MOD, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LSTRT, KC_PRVWD, KC_NXTWD, KC_LEND, KC_DLINE, KC_PGUP, KC_PGDN, \
+        KC_CAPS,     RGB_MOD, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LSTRT, KC_PRVWD, KC_NXTWD, KC_LEND, KC_DLINE, KC_PGDN, KC_PGUP, \
         _______,       KC_LEFT, KC_DOWN, KC_RGHT, KC_ABHOM, KC_LSTRT, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, KC_END,  KC_ABEND,          KC_L, \
         _______,          BL_DEC,  KC_DEL,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_QWERTY, KC_COLEMAK, _______, _______, \
         _______,  _______, _______,                            KC_L,                          _______,_______, _______, KC_APP, _______

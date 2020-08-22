e805f2a855b47013bd9f79145cde2b50d0137166
Gus Kohtz <1nd13r0ck@gmail.com>
2020-08-22 13:12:41 -0500

    add media controls to arrow cluster; organize setting;

--- keyboards/dztech/dz60rgb/keymaps/fatsug/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/fatsug/keymap.c
@@ -107,8 +107,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______, \
         _______,     _______, _______, _______, _______, _______, _______, _______, KC_INS,  _______, KC_PSCR, _______, _______,  _______, \
         _______,       KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,            KC_R, \
-        _______,          KC_MPLY, KC_MPRV,  KC_MNXT, _______, _______, _______, _______, _______, KC_MS_BTN1, KC_MS_BTN2,_______, _______, \
-        _______,  _______, _______,                            KC_R,                         _______, _______, _______, _______, _______
+        _______,          KC_MPLY, KC_MPRV,  KC_MNXT, _______, _______, _______, _______, KC_MS_BTN1, KC_MS_BTN2,KC_MPLY, KC_VOLU, KC_MUTE, \
+        _______,  _______, _______,                            KC_R,                           _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
     ),
 
 	//[4] = LAYOUT(KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, LCTL_T(KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH), KC_UP, LT(2,KC_DEL), KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_RALT, TO(0), KC_LEFT, KC_DOWN, KC_RGHT)
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TGOS,     XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_MOD, RGB_TOG, \
         XXXXXXX,       XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         EEP_RST, \
         XXXXXXX,          XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_QWERTY, KC_COLEMAK, KC_ASTG, XXXXXXX, XXXXXXX, \
-        _______,  _______, _______,                            KC_A,                        _______, _______, _______, _______, _______
+        _______,  _______, _______,                            KC_A,                           _______, _______, _______, _______, _______
     )
 };
 
--- keyboards/dztech/dz60rgb/keymaps/fatsug/rules.mk
+++ keyboards/dztech/dz60rgb/keymaps/fatsug/rules.mk
@@ -1,10 +1,14 @@
 NKRO_ENABLE = yes                 # USB Nkey Rollover
-# AUTO_SHIFT_ENABLE = yes      # Auto Shift
 # VELOCIKEY_ENABLE = yes
 
 EXTRAFLAGS += -flto
 
 # SRC += dz60rgb.c
 
+# # # # # # # # #
+#
 # Customizations
+#
+
 LEADER_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes      # Auto Shift

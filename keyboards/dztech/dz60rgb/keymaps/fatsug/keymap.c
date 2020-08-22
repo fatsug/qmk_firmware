fc643456445aef43e9485534dadb567fbd7a7c2c
Gus Kohtz <1nd13r0ck@gmail.com>
2020-08-22 13:21:41 -0500

    update keymap representation to dz60 layout; fix delete key to switch to _Lower layer if held

--- keyboards/dztech/dz60rgb/keymaps/fatsug/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/fatsug/keymap.c
@@ -69,7 +69,7 @@ enum custom_keycodes {
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+--------|
  * |  Lower   |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |  ;:  |  '"  |    Ent    |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+--------|
- * |    LShift   |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |    RShift     |
+ * |    LShift   |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |/? RShift| Up  | de     |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+--------|
  * |  LCTL | LGUI  |  LALT  |               S P A C E                   | Lower| Raise|Left|Down| Right|
  * .------+------+------+------+------+------|------+------+------+------+------+------+------+--------.
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,       KC_BSPC, \
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS, \
         KC_LOWER,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT, \
-        KC_LSFT,          KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, RSFT_T(KC_SLSH), KC_UP, LT(2,KC_DEL), \
+        KC_LSFT,          KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, RSFT_T(KC_SLSH), KC_UP, LT(_LOWER,KC_DEL), \
         KC_LCTL,  KC_LGUI, KC_LALT,                        KC_SPC,                             KC_LOWER, KC_RAISE, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 	//[1] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, KC_O, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_K, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),

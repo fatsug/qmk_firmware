d14812b5ed1519dadfc803c6d08cf4604c9f12db
Gus Kohtz <1nd13r0ck@gmail.com>
2020-12-19 12:12:56 -0600

    add new Navigation layer

--- keyboards/dztech/dz60rgb/keymaps/fatsug/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/fatsug/keymap.c
@@ -15,6 +15,7 @@ enum custom_keycodes {
   KC_LOWER,
   KC_RAISE,
   KC_ADJUST,
+  KC_NAVI,
   KC_ISMAC,
   KC_TGOS,
   KC_PRVWD,
@@ -59,6 +60,7 @@ enum custom_keycodes {
 #define _LOWER 2
 #define _RAISE 3
 #define _ADJUST 4
+#define _NAVI 5
 
 /*
  * QWERTY
@@ -80,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,       KC_BSPC, \
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS, \
         KC_LOWER,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT, \
-        KC_LSFT,          KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, RSFT_T(KC_SLSH), KC_UP, LT(_LOWER,KC_DEL), \
+        KC_LSFT,          KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, RSFT_T(KC_SLSH), KC_UP, LT(_NAVI,KC_DEL), \
         KC_LCTL,  KC_LGUI, KC_LALT,                        KC_SPC,                             KC_LOWER, KC_RAISE, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
@@ -114,7 +116,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,       XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         EEP_RST, \
         XXXXXXX,          XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_QWERTY, KC_COLEMAK, KC_ASTG, KC_ASUP, KC_ASRP, \
         _______,  _______, _______,                            KC_A,                           _______, _______, _______, KC_ASDN, _______
+    ),
+
+    [_NAVI] = LAYOUT(
+        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, \
+        XXXXXXX,     XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, \
+        XXXXXXX,       XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, \
+        XXXXXXX,          XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, KC_PGUP, XXXXXXX, \
+        _______,  _______, _______,                            KC_N,                        KC_ABHOM, KC_ABEND, KC_LSTRT, KC_PGDN, KC_LEND
     )
+
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {

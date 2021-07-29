9db56a2736a4e69965d1109dd4898f0fbe5cba98
Gus Kohtz <1nd13r0ck@gmail.com>
2021-07-29 11:35:53 -0500

    add mouse scrolling to right encoder

--- keyboards/sofle/keymaps/fatsug/keymap.c
+++ keyboards/sofle/keymaps/fatsug/keymap.c
@@ -419,17 +419,34 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #ifdef ENCODER_ENABLE
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
+    if (index == 0) {  // left
         if (clockwise) {
             tap_code(KC_VOLD);
         } else {
             tap_code(KC_VOLU);
         }
-    } else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_PGDOWN);
-        } else {
-            tap_code(KC_PGUP);
+    }
+    else if (index == 1) {  // right
+        if (IS_LAYER_ON(_LOWER))
+        {
+            if (clockwise)
+            {
+                tap_code(KC_PGUP);
+            } else
+            {
+                tap_code(KC_PGDOWN);
+            }
+        }
+        else
+        {
+            if (clockwise)
+            {
+                tap_code(KC_MS_WH_DOWN);
+            }
+            else
+            {
+                tap_code(KC_MS_WH_UP);
+            }
         }
     }
 }

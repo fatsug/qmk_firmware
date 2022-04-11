88a1d177b254c93ed1cf76d1c018efc43fdb7e5e
gus <gus.kohtz@outlook.com>
2022-04-10 20:06:32 -0500

    dz60: add media controls and insert and print screen

--- keyboards/dz60/keymaps/fatsug/keymap.c
+++ keyboards/dz60/keymaps/fatsug/keymap.c
@@ -82,10 +82,10 @@ enum custom_keycodes {
 
 [_RAISE] = LAYOUT(
         KC_ISMAC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
-        KC_TGOS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, XXXXXXX,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EEP_RST,
-        _______, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,                   _______, _______, _______,          _______, _______, _______, _______, _______
+        KC_TGOS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _______, KC_INS,  _______, KC_PSCR, _______, _______, XXXXXXX,
+        RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EEP_RST,
+        RGB_VAD, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______, _______, KC_MPLY, KC_VOLU, KC_MUTE,
+        _______, _______, _______,                   _______, _______, _______,          _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
 ),
 
 [_ADJUST] = LAYOUT(

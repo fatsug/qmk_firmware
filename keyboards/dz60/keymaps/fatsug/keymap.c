699c9f87974f853844ffdc7f09de14af3a105aef
gus <gus.kohtz@outlook.com>
2022-04-17 11:30:32 -0500

    found thaf billiams keymaps has a working right shift. This is the last commit with my non-working version.

--- keyboards/dz60/keymaps/fatsug/keymap.c
+++ keyboards/dz60/keymaps/fatsug/keymap.c
@@ -68,13 +68,13 @@ enum custom_keycodes {
         KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LOWER,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, KC_UP, LT(_NAVI,KC_DEL),
+        KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  MT(MOD_RSFT,KC_SLSH), KC_UP, LT(_NAVI,KC_DEL),
         KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,  KC_SPC,  KC_SPC,           KC_LOWER, KC_RAISE,     KC_LEFT, KC_DOWN, KC_RGHT
  ),
 
 [_LOWER] = LAYOUT(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,
-        KC_CAPS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, RESET,
+        KC_CAPS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, XXXXXXX,
         _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,
         _______, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______, _______, KC_SLSH, KC_PGUP, _______,
         _______, _______, _______,                   _______, _______, _______,          _______, _______, KC_LSTRT, KC_PGDN, KC_LEND
@@ -83,7 +83,7 @@ enum custom_keycodes {
 [_RAISE] = LAYOUT(
         KC_ISMAC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
         KC_TGOS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _______, KC_INS,  _______, KC_PSCR, _______, _______, XXXXXXX,
-        RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EEP_RST,
+        RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         RGB_VAD, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______, _______, KC_MPLY, KC_VOLU, KC_MUTE,
         _______, _______, _______,                   _______, _______, _______,          _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
 ),
@@ -99,7 +99,7 @@ enum custom_keycodes {
 [_NAVI] = LAYOUT(
         KC_ISMAC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
         KC_TGOS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, XXXXXXX,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EEP_RST,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          XXXXXXX,
         _______, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______, _______, _______, KC_ABHOM, _______,
         _______, _______, _______,                   _______, _______, _______,          _______, _______, _______, KC_ABEND, _______
 )

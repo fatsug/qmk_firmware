e53df30b201cf6abc9cadde044950c316353949a
gus <gus.kohtz@outlook.com>
2022-08-09 20:52:30 -0500

    fix sofle build error with encoder_update_user function

--- keyboards/sofle/keymaps/fatsug/keymap.c
+++ keyboards/sofle/keymaps/fatsug/keymap.c
@@ -418,7 +418,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 #ifdef ENCODER_ENABLE
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {  // left
         if (clockwise) {
             tap_code(KC_VOLD);
@@ -449,6 +449,8 @@ void encoder_update_user(uint8_t index, bool clockwise) {
             }
         }
     }
+
+    return true;
 }
 
 #endif
